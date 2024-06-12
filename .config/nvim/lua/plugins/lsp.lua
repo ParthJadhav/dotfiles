e6d2a4976233c728eb62b10e6a28a5365c68ba82
Parth Jadhav <pjadhav@atlassian.com>
2024-06-12 20:59:39 +0530

    ts

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -49,7 +49,17 @@ return {
         cmd = { "LspInstall", "LspUninstall" },
         opts = function(_, opts)
           if not opts.handlers then opts.handlers = {} end
-          opts.handlers[1] = function(server) require("astronvim.utils.lsp").setup(server) end
+          opts.handlers[1] = function(server)
+            require("astronvim.utils.lsp").setup(server, {
+              on_attach = function(client, bufnr)
+                local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+                if ft == "rust" or ft == "typescript" then
+                  client.resolved_capabilities.document_formatting = true
+                end
+              end
+            })
+          end
+          opts.ensure_installed = { "rust_analyzer", "tsserver" }
         end,
         config = require "plugins.configs.mason-lspconfig",
       },
@@ -59,18 +69,6 @@ return {
     end,
     event = "User AstroFile",
     config = require "plugins.configs.lspconfig",
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    dependencies = {
-      {
-        "jay-babu/mason-null-ls.nvim",
-        cmd = { "NullLsInstall", "NullLsUninstall" },
-        opts = { handlers = {} },
-      },
-    },
-    event = "User AstroFile",
-    opts = function() return { on_attach = require("astronvim.utils.lsp").on_attach } end,
   },
   {
     "stevearc/aerial.nvim",
